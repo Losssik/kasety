2cff29d4a2af7f2009026f784135603b8972f250
Losssik <you@example.com>
2024-01-03 20:48:44 +0100

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <h1>hello world</h1>;
+  return (
+    <div>
+      <h1>hello world</h1>
+      <p> just for testinh</p>
+    </div>
+  );
 }
 
 export default App;
